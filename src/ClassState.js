57d767588043c1533228a4ade7c0bb5ab54a3a93
Jorge Sevilla <jorge.garcia@compasso.com.br>
2022-08-30 10:08:51 -0300

    Métodos del ciclo de vida en React.Component con clases

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,33 +1,52 @@
 import React from "react";
+import { Loading } from './Loading';
 
 class ClassState extends React.Component {
-   
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            error: true
-        };
-    }
+    this.state = {
+      error: true,
+      loading: false,
+    };
+  }
 
-    render(){
-        return(
-          <div>
-            <h2>Eliminar {this.props.name}</h2>
-            <p>Por favor, escribe el código de seguridad</p>
+  /*UNSAFE_componentWillMount(){
 
-            {this.state.error && (
-                <p>Error: el código es incorrecto</p>
-            )}
+  }*/
+  
+  /*componentDidMount(){
 
-            <input placeholder="Código de seguridad"></input>
-            <button
-                onClick={() => this.setState(prevState => ({ error: !prevState.error }))}
-            >
-                Comprobar
-            </button>
-          </div>  
-        );
+  }*/
+
+  componentDidUpdate(){
+    if (!!this.state.loading) {
+        setTimeout(() => {
+          this.setState({ loading: false });
+        }, 3000);
+      }
+    };
+
+  render() {
+    return (
+      <div>
+        <h2>Eliminar {this.props.name}</h2>
+        <p>Por favor, escribe el código de seguridad</p>
+
+        {this.state.error && <p>Error: el código es incorrecto</p>}
+
+        {this.state.loading && <Loading />}
+
+        <input placeholder="Código de seguridad"></input>
+        <button
+          onClick={() =>
+            this.setState(({ loading: true }))
+          }
+        >
+          Comprobar
+        </button>
+      </div>
+    );
     }
 }
 
