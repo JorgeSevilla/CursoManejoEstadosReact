34e3005b05ab4c646a0e76899eb7b614ea2cd2eb
Jorge Sevilla <jorge.garcia@compasso.com.br>
2022-08-31 12:14:30 -0300

    Estados imperativos com useState

--- src/UseState.js
+++ src/UseState.js
@@ -7,6 +7,8 @@ function UseState({ name }) {
         value: '', 
         error: false, 
         loading: false,
+        deleted: false, 
+        confirmed: false,
     })
 
   console.log(state);
@@ -18,7 +20,8 @@ function UseState({ name }) {
             setState({
                 ...state,
                 error: false,
-                loading: false
+                loading: false,
+                confirmed: true,
             });
           //setLoading(false);
           //setError(true);
@@ -35,41 +38,88 @@ function UseState({ name }) {
     }
   }, [state.loading]);
 
-  return (
-    <div>
-      <h2>Eliminar {name}</h2>
-      <p>Por favor, escribe el código de seguridad</p>
-
-      {state.error && !state.loading && <p>Error: el código es incorrecto</p>}
-
-      {state.loading && <p>Cargando...</p>}
-
-      <input
-        placeholder="Código de seguridad"
-        value={state.value}
-        onChange={(event) => {
-            setState({
-                ...state,
-                value: event.target.value,
-            });
-          //setError(false); //Aqui recarga repetidamente
-          //setValue(event.target.value);
-        }}
-      />
-      <button
-        onClick={() => {
-            setState({
-                ...state,
-                loading: true,
-            });
-          //setError(false); //Es una solución
-          //setLoading(true);
-        }}
-      >
-        Comprobar
-      </button>
-    </div>
-  );
+ if(!state.deleted && !state.confirmed){
+    return (
+        <div>
+          <h2>Eliminar {name}</h2>
+          <p>Por favor, escribe el código de seguridad</p>
+    
+          {state.error && !state.loading && <p>Error: el código es incorrecto</p>}
+    
+          {state.loading && <p>Cargando...</p>}
+    
+          <input
+            placeholder="Código de seguridad"
+            value={state.value}
+            onChange={(event) => {
+                setState({
+                    ...state,
+                    value: event.target.value,
+                });
+              //setError(false); //Aqui recarga repetidamente
+              //setValue(event.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+                setState({
+                    ...state,
+                    loading: true,
+                });
+              //setError(false); //Es una solución
+              //setLoading(true);
+            }}
+          >
+            Comprobar
+          </button>
+        </div>
+      );
+ }else if(!!state.confirmed && !state.deleted){
+    return(
+        <React.Fragment>
+            <p>Está seguro?</p>
+            <button
+                onClick={() =>{
+                    setState({
+                        ...state,
+                        deleted: true,
+                    });
+                }}
+            >
+                Si, eliminar
+            </button>
+            <button
+                onClick={() =>{
+                    setState({
+                        ...state,
+                        confirmed: false,
+                        value: '',
+                    });
+                }}
+            >
+                Cancelar
+            </button>
+        </React.Fragment>
+    );
+ }else {
+    return(
+        <React.Fragment>
+            <p>Eliminado con éxito</p>
+            <button
+                onClick={() =>{
+                    setState({
+                        ...state,
+                        confirmed: false,
+                        deleted: false,
+                        value: ''
+                    });
+                }}
+            >
+                Volver atrás
+            </button>
+        </React.Fragment>
+    );
+ }
 }
 
 export { UseState };
