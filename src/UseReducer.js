e5ee40a7289215ab6a9d8fa3019d21ca7efebcd4
Jorge Sevilla <jorge.garcia@compasso.com.br>
2022-09-02 10:47:25 -0300

    Manejo de estados de componentes de forma declarativa con useReducer

--- src/UseReducer.js
+++ src/UseReducer.js
@@ -1,5 +1,3 @@
-import { type } from "@testing-library/user-event/dist/type";
-
 /**
  * ¿Qué es un reducer?
 
@@ -27,6 +25,92 @@ El reducer define los posibles estados por donde pasara nuestra App.
 Con action type elegimos cual de esos estados queremos disponer con el cambio o evento del usuario.
 Con action payload damos dinamismo a dicho estado. Será el mismo estado pero le daremos características especiales
  */
+import React from "react";
+
+const SECURITY_CODE = "paradigma";
+
+function UseReducer({ name }) {
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  console.log(state);
+
+  React.useEffect(() => {
+    if (!!state.loading) {
+      setTimeout(() => {
+
+        if (state.value === SECURITY_CODE) {
+            dispatch({ type: 'CONFIRM'});
+
+        } else {
+            dispatch({ type: 'ERROR'});
+        }
+      }, 3000);
+    }
+  }, [state.loading]);
+
+ if(!state.deleted && !state.confirmed){
+    return (
+        <div>
+          <h2>Eliminar {name}</h2>
+          <p>Por favor, escribe el código de seguridad</p>
+    
+          {state.error && !state.loading && <p>Error: el código es incorrecto</p>}
+    
+          {state.loading && <p>Cargando...</p>}
+    
+          <input
+            placeholder="Código de seguridad"
+            value={state.value}
+            onChange={(event) => {
+                dispatch({ type: 'WRITE', payload: event.target.value});
+
+            }}
+          />
+          <button
+            onClick={() => {
+                dispatch({ type: 'CHECK'});
+            }}
+          >
+            Comprobar
+          </button>
+        </div>
+      );
+ }else if(!!state.confirmed && !state.deleted){
+    return(
+        <React.Fragment>
+            <p>Está seguro?</p>
+            <button
+                onClick={() =>{
+                    dispatch({ type: 'DELETE'});
+                }}
+            >
+                Si, eliminar
+            </button>
+            <button
+                onClick={() =>{
+                    dispatch({ type: 'RESET'});
+                }}
+            >
+                Cancelar
+            </button>
+        </React.Fragment>
+    );
+ }else {
+    return(
+        <React.Fragment>
+            <p>Eliminado con éxito</p>
+            <button
+                onClick={() =>{
+                    dispatch({ type: 'RESET'});                   
+                }}
+            >
+                Volver atrás
+            </button>
+        </React.Fragment>
+    );
+ }
+}
+
 const initialState = {
     value: '', 
     error: false, 
@@ -42,7 +126,7 @@ const initialState = {
 
 //}
 
-const reducerIf = (state, action) => {
+/*const reducerIf = (state, action) => {
     if(action.type === 'ERROR'){
         return{
             ...state,
@@ -83,26 +167,52 @@ const reducerSwitch = (state, action) =>{
                 ...state,
             };
     }
-}
+}*/
 
 //Tercera forma
-const reducerObject = (state) =>({
+const reducerObject = (state, payload) =>({
+    'CONFIRM':{
+        ...state,
+        error: false,
+        loading: false,
+        confirmed: true,
+    },
+
     'ERROR':{
         ...state,
         error: true,
         loading: false, 
     },
 
+    'WRITE':{
+        ...state, 
+        value: payload
+    },
+
     'CHECK':{
         ...state,
         loading: true, 
     },
+
+    'DELETE':{
+        ...state,
+        deleted: true,
+    },
+
+    'RESET':{
+        ...state,
+        confirmed: false,
+        deleted: false,
+        value: '',
+    }
 });
 
 const reducer = (state, action) =>{
     if(reducerObject(state)[action.type]){
-        return reducerObject(state)[action.type];
+        return reducerObject(state, action.payload)[action.type];
     }else {
         return state;
     }
 };
+
+export { UseReducer };
